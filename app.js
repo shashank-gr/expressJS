f021d6a4f012f67a85e3ab1ea4156ea586d5941c
shashank4214@gmail.com <shashank-gr>
2022-11-06 19:39:29 +0530

    using body-parser

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"></br><input type="number" name="quantity"/></br><button type="submit">submit product</button></form>'
+  );
+});
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("Hello from Express JS");
 });
 app.listen(3500);
